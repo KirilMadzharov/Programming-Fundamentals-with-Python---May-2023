a1e67b657d294bf37d19aa4e044efb68fe2ea65c
Kiril Madzharov <133707345+KirilMadzharov@users.noreply.github.com>
2023-10-26 04:17:44 +0100

    Delete 02. Data Types and Variables/3. More Exercise directory

--- 02. Data Types and Variables/3. More Exercise/t_3_decrypting_messages.py
+++
@@ -1,7 +0,0 @@
-key = int(input())
-number_of_lines = int(input())
-
-for line in range(number_of_lines):
-    letter = input()
-    decrypted_letter = ord(letter) + key
-    print(f'{chr(decrypted_letter)}', end='')
